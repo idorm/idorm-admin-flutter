9b0a5d824ac2cb724632208877c38b6b1fa9bb44
altpfwlzh <altpfwlzh@gmail.com>
2023-09-28 01:38:19 +0900

    FEAT : 간격 조절용 _padding Widget 완성

--- lib/presentation/page/Detail/detail.dart
+++ lib/presentation/page/Detail/detail.dart
@@ -1,21 +1,24 @@
-/*
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:idorm_admin_flutter/presentation/common/colors.dart';
-import 'package:idorm_admin_flutter/presentation/page/home/home_controller.dart';
+import 'package:idorm_admin_flutter/presentation/page/Detail/detaiController.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+
+class Detail extends StatefulWidget {
+
+  const Detail({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  State<Detail> createState() => _DetailState();
 }
 
-class _HomeState extends State<Home> {
-  HomeController homeController = Get.find();
+class _DetailState extends State<Detail> {
+
+  DetailController detailController = Get.put(DetailController());
   WebViewController? _webViewController;
 
   @override
@@ -29,52 +32,24 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
-      body: Column(
-          children :[
-            Padding(
-                padding: EdgeInsets.only(bottom: 8),
-                child: Text("이거 되면 성공",
-                    style: TextStyle(color: IDormColors.primary))),
-            SizedBox(
-              height: 500,
-              child: InAppWebView(
-                initialUrlRequest: URLRequest(
-                  url: Uri.parse('https://bioeng.inu.ac.kr/dorm/6528/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGZG9ybSUyRjE1MjElMkYzNzI3NTElMkZhcnRjbFZpZXcuZG8lM0ZwYWdlJTNEMSUyNnNyY2hDb2x1bW4lM0RzaiUyNnNyY2hXcmQlM0QlMjZiYnNDbFNlcSUzRCUyNmJic09wZW5XcmRTZXElM0QlMjZyZ3NCZ25kZVN0ciUzRCUyNnJnc0VuZGRlU3RyJTNEJTI2aXNWaWV3TWluZSUzRGZhbHNlJTI2cGFzc3dvcmQlM0QlMjY%3D'),
-                ),
-                initialOptions: InAppWebViewGroupOptions(
-                    android: AndroidInAppWebViewOptions(useHybridComposition: true)),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(bottom: 8),
-                child: Text("이거 되면 성공",
-                    style: TextStyle(color: IDormColors.primary))),
-          ]
+      body: Padding(
+        padding: EdgeInsets.only(left: 24, right: 24, top: 60, bottom: 30),
+        child: Column(
+            children :[
+              _webView(),
+              _padding(30),
+              _isPublic(true),
+              _padding(30),
+
+            ]
+        ),
       ),
     );
   }
 
-  AppBar appBar() {
-    return AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            Get.back();
-          },
-        ),
-        iconTheme: const IconThemeData(color: Colors.black),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: const Text(
-          "테스트 페이지",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ));
+  Widget _padding(double height){
+    return SizedBox(height: height,);
   }
 
+
 }
-*/
