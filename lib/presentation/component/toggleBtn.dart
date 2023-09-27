8a163b6a91c70c4783b438d4f553ee918b1146f8
altpfwlzh <altpfwlzh@gmail.com>
2023-09-28 03:04:17 +0900

    FEAT : 토글 버튼 생성

---
+++ lib/presentation/component/toggleBtn.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ToggleBtn extends StatefulWidget {
+  ToggleBtn({Key? key, required this.isPublicChecked, required this.action}) : super(key: key);
+
+  bool isPublicChecked;
+  Function() action;
+
+  @override
+  State<ToggleBtn> createState() => _ToggleBtnState();
+}
+
+class _ToggleBtnState extends State<ToggleBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(value: widget.isPublicChecked,onChanged: (value)  {
+      setState(() {
+        widget.isPublicChecked = value;
+        widget.action();
+      });
+    },);
+  }
+}
