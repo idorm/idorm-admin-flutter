796f2e1b411b2dc011f4d0d5e21044329db3aa74
altpfwlzh <altpfwlzh@gmail.com>
2023-09-28 03:26:38 +0900

    FEAT : ListView.builder 확장 가능하게 하기
    Expand로 감싸고 Column될 수 있게 함

--- lib/presentation/page/home/home.dart
+++ lib/presentation/page/home/home.dart
@@ -3,8 +3,8 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:idorm_admin_flutter/presentation/common/colors.dart';
 import 'package:idorm_admin_flutter/presentation/page/home/home_controller.dart';
+import '../../component/toggleBtn.dart';
 import '../Detail/detail.dart';
-
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,6 +16,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   HomeController homeController = Get.find();
 
+  final bool _isPublicChecked = false;
+
   @override
   void initState() {
     super.initState();
@@ -25,63 +27,74 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index){
-              return _noticeList();
-            }),
-      ),
-    );
-  }
-
-  Widget _noticeList(){
-    return Padding(
-      padding: const EdgeInsets.only(top: 22, bottom: 14, right: 24, left: 24),
-      child: GestureDetector(
-        onTap: () => Get.to(Detail()),
-        child: Container(
-          padding: const EdgeInsets.only(top: 24, bottom: 22, right: 22, left: 22),
-          height: 160,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(14),
-              border: Border.all(color: IDormColors.gray2, width: 1),
-              color: IDormColors.white
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "공지 사항 제목",
-                style: TextStyle(
-                  fontWeight: FontWeight.w400, fontSize: 14, color: IDormColors.black),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 5)),
-              const Text(
-                "6월 18일",
-                style: TextStyle(
-                    fontWeight: FontWeight.w400, fontSize: 14, color: IDormColors.black),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 20)),
-              const Text(
-                "일정이 있는 기숙사",
-                style: TextStyle(
-                    fontWeight: FontWeight.w400, fontSize: 12, color: IDormColors.gray3),
-              ),
-              _dorm(true, true, true)
-            ],
-          ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 22, bottom: 14, right: 24, left: 24),
+          child: Column(children: [
+            _isPublic(),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _noticeList();
+                  }),
+            ),
+          ]),
         ),
       ),
     );
   }
 
-  Widget _dorm(bool dorm1, bool dorm2, bool dorm3){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children : [
-        Padding(
-        padding: const EdgeInsets.only(top: 3,bottom: 3, right: 7),
+
+
+  Widget _noticeList() {
+    return GestureDetector(
+      onTap: () => Get.to(Detail()),
+      child: Container(
+        padding:
+            const EdgeInsets.only(top: 24, bottom: 22, right: 22, left: 22),
+        height: 160,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14),
+            border: Border.all(color: IDormColors.gray2, width: 1),
+            color: IDormColors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "공지 사항 제목",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14,
+                  color: IDormColors.black),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 5)),
+            const Text(
+              "6월 18일",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14,
+                  color: IDormColors.black),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20)),
+            const Text(
+              "일정이 있는 기숙사",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: IDormColors.gray3),
+            ),
+            _dorm(true, true, true)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _dorm(bool dorm1, bool dorm2, bool dorm3) {
+    return Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+      Padding(
+        padding: const EdgeInsets.only(top: 3, bottom: 3, right: 7),
         child: Container(
           width: 60,
           height: 26,
@@ -92,54 +105,54 @@ class _HomeState extends State<Home> {
               child: Text(
                 "1기숙사",
                 style: TextStyle(
-                    fontWeight: FontWeight.w400, fontSize: 12, color: IDormColors.gray4),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: IDormColors.gray4),
               ),
             ),
           ),
         ),
       ),
-        Padding(
-          padding: const EdgeInsets.only(top: 3,bottom: 3, right: 7),
-          child: Container(
-            width: 60,
-            height: 26,
-            color: IDormColors.gray2,
-            child: Visibility(
-              visible: dorm1,
-              child: const Center(
-                child: Text(
-                  "2기숙사",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w400, fontSize: 12, color: IDormColors.gray4),
-                ),
+      Padding(
+        padding: const EdgeInsets.only(top: 3, bottom: 3, right: 7),
+        child: Container(
+          width: 60,
+          height: 26,
+          color: IDormColors.gray2,
+          child: Visibility(
+            visible: dorm1,
+            child: const Center(
+              child: Text(
+                "2기숙사",
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: IDormColors.gray4),
               ),
             ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(top: 3,bottom: 3, right: 7),
-          child: Container(
-            width: 60,
-            height: 26,
-            color: IDormColors.gray2,
-            child: Visibility(
-              visible: dorm1,
-              child: const Center(
-                child: Text(
-                  "3기숙사",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w400, fontSize: 12, color: IDormColors.gray4),
-                ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 3, bottom: 3, right: 7),
+        child: Container(
+          width: 60,
+          height: 26,
+          color: IDormColors.gray2,
+          child: Visibility(
+            visible: dorm1,
+            child: const Center(
+              child: Text(
+                "3기숙사",
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: IDormColors.gray4),
               ),
             ),
           ),
         ),
-
-
-      ]
-    );
+      ),
+    ]);
   }
-
-
-
 }
